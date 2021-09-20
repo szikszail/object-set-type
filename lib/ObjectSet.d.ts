32c4bc5957bde92abcd440860f01413e1a92e5c0
Laszlo Szikszai <laszlo_szikszai@epam.com>
2021-09-20 17:09:54 +0200

    Added type definition

---
+++ lib/ObjectSet.d.ts
@@ -0,0 +1,17 @@
+interface ObjectSet<T> {
+    constructor(iterable: Iterable<T>);
+    public has(needle: T): boolean;
+    public delete(item: T): boolean;
+    public add(item?: T): ObjectSet<T>
+    public clear(): void;
+    public [Symbol.iterator](): Iterator<T>;
+    public keys(): Iterator<T>;
+    public values(): Iterator<T>;
+    public entries(): Iterator<[T, T]>;
+    public forEach(callbackFn: (item: T, i: number, items: T[]) => void, thisArg: ObjectSet<T>): void;
+
+    get size(): number;
+
+}
+
+export = ObjectSet;
